86b0a525090edb4f279da911a48f17ea2f74aa4c
[S] <nargno.s@gmail.com>
2016-12-15 15:26:08 +0800

    更新了一个转换类
    以后用的过程中功能会慢慢补充

---
+++ DataRowConvert/ConvertorEmit.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataRowConvert
+{
+    public static class ConvertorEmit
+    {
+        // 对应目标的映射关系
+        [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = false)]
+        public sealed class ConvertFieldAttribute : Attribute
+        {
+            readonly string fieldName_;
+            public ConvertFieldAttribute(string fieldName)
+            {
+                fieldName_ = fieldName;
+            }
+
+            public string ColumnName
+            {
+                get { return fieldName_; }
+            }
+        }
+        public static Action<DataRow,TResult> EmitFillRow<TResult>()
+        {
+            var typeResult = typeof(TResult);
+            var typeDataRow = typeof(DataRow);
+
+            var row = Expression.Parameter(typeDataRow, "row");
+            var obj = Expression.Parameter(typeResult, "obj");            
+
+            // row.SetField(colName, obj.field)
+            var assigns = from property in typeResult.GetProperties()
+                          let convertFields = GetConvertFieldAttrs(property)
+                          where CheckConvertFieldAttr(convertFields)
+                          let columnName = GetColName(convertFields)
+                          let field = GetProp(obj, property)
+                          select AssignRowField(row, columnName, field, property.PropertyType);
+
+            return Expression.Lambda<Action<DataRow, TResult>>(Expression.Block(assigns), row, obj).Compile();
+        }
+
+        private static MethodCallExpression AssignRowField(ParameterExpression paramRow, string columnName, Expression value, Type valueType)
+        {
+            return Expression.Call(typeof(DataRowExtensions), "SetField", new Type[] { valueType }, paramRow, MakeConstStr(columnName), value);
+        }
+
+        // 生成转换函数, 需要预先定义列名
+        public static Func<DataRow, TResult> EmitRowConvert<TResult>()
+            where TResult : new()
+        {
+            var typeResult = typeof(TResult);
+            var typeDataRow = typeof(DataRow);
+            
+            var param = Expression.Parameter(typeDataRow, "row");
+            // new TResult()
+            var newObj = Expression.New(typeResult);
+            // TResult ret
+            var result = Expression.Variable(typeResult, "ret");
+            // TResult ret = new TResult();
+            var initResult = Expression.Assign(result, newObj);
+            // ret.Field(columnName)
+            var assigns = from property in typeResult.GetProperties()
+                          let convertFields = GetConvertFieldAttrs(property)
+                          where CheckConvertFieldAttr(convertFields)
+                          let columnName = GetColName(convertFields)
+                          let field = GetProp(result, property)
+                          let fieldValue = GetRowField(param, columnName, property.PropertyType)
+                          select Expression.Assign(field, fieldValue);
+            var blocks = Expression.Block(
+                new ParameterExpression[] { result },
+                new Expression[] { initResult }.Concat(assigns).Concat(new Expression[] { result }));
+
+            return Expression.Lambda<Func<DataRow, TResult>>(blocks, param).Compile();
+        }
+
+        private static MethodCallExpression GetRowField(ParameterExpression paramRow, string columnName, Type valueType)
+        {
+            return Expression.Call(typeof(DataRowExtensions), "Field", new Type[] { valueType }, paramRow, MakeConstStr(columnName));
+        }
+
+        private static ConstantExpression MakeConstStr(string str)
+        {
+            return Expression.Constant(str, typeof(string));
+        }
+
+        private static MemberExpression GetProp(ParameterExpression result, PropertyInfo property)
+        {
+            return Expression.Property(result, property.Name);
+        }
+
+        private static string GetColName(object[] convertFields)
+        {
+            return (convertFields.First() as ConvertFieldAttribute).ColumnName;
+        }
+
+        private static bool CheckConvertFieldAttr(object[] convertFields)
+        {
+            return convertFields.Count() == 1 && convertFields.First() is ConvertFieldAttribute;
+        }
+
+        private static object[] GetConvertFieldAttrs(PropertyInfo property)
+        {
+            return property.GetCustomAttributes(typeof(ConvertFieldAttribute), true);
+        }
+    }
+}
---
+++ DataRowConvertTest/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Linq;
+using DataRowConvert;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace DataRowConvertTest
+{
+    struct MyStruct
+    {
+        [ConvertorEmit.ConvertField("ID")]
+        public int? ID { get; set; }
+        [ConvertorEmit.ConvertField("Value")]
+        public string Text { get; set; }
+        public int? Ignore { get; set; }
+        // 可以设置多变量接收数据，作为赋值到row的话会保留最后一个重复数据
+        [ConvertorEmit.ConvertField("Value")]
+        public string TextCopy { get; set; }
+    }
+    struct MyStruct2
+    {
+        [ConvertorEmit.ConvertField("ID")]
+        public int? A { get; set; }
+        [ConvertorEmit.ConvertField("Value")]
+        public string B { get; set; }
+        // [ConvertorEmit.ConvertField("Value")]
+        public string C { get; set; }
+    }
+    [TestClass]
+    public class DataRowConvertTest
+    {
+        [TestMethod]
+        public void TestEmitConvertor()
+        {
+            var rowToStruct = ConvertorEmit.EmitRowConvert<MyStruct>();
+            var fillRow = ConvertorEmit.EmitFillRow<MyStruct2>();
+
+            DataTable tb = new DataTable();
+            tb.Columns.Add("ID", typeof(int));
+            tb.Columns.Add("Value", typeof(string));
+
+            List<MyStruct2> addList = new List<MyStruct2>()
+            {
+                new MyStruct2() { A=null, B="================", C=null },
+                new MyStruct2() { A=99999, B="================", C="ignore" },
+                new MyStruct2() { A=00, B="Hello", C="ignore" },
+                new MyStruct2() { A=2, B="!!", C="ignore" },
+                new MyStruct2() { A=1, B="World", C="ignore" },
+            };
+
+            int addSum = 0;
+            // 不同结构不同字段只要设置特性就可以互用
+            foreach (var item in addList)
+            {
+                addSum += item.A.GetValueOrDefault();
+                var row = tb.NewRow();
+                fillRow(row, item);
+                tb.Rows.Add(row);
+            }
+
+            var objs = from DataRow row in tb.Rows let obj = rowToStruct(row) orderby obj.ID select obj;
+            var sum = objs.Sum((obj) => obj.ID.GetValueOrDefault());
+            Assert.IsTrue(addSum == sum);
+            var output = string.Join(" ", from text in objs select text.Text);
+
+            Debug.WriteLine(output);
+        }
+    }
+}
